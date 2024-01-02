5c81b479c24549571e12082d06ecd0fd4acaf6a8
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-01-02 17:17:16 +0000

    added BetterErrors lines from lesson

--- dice.rb
+++ dice.rb
@@ -3,6 +3,10 @@ require "sinatra/reloader"
 require "better_errors"
 require "binding_of_caller"
 
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
 get("/") do
   "<h1> Dice Roll </h1>
   <ul>
