9e03748b00bca6e5a91db8fdb4ce01d4121cb74b
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-01-02 17:11:56 +0000

    added 5d4 roller

--- dice.rb
+++ dice.rb
@@ -49,3 +49,17 @@ get("/dice/1/20") do
   "<h1>1d20</h1>
    <p>#{outcome}</p>"
 end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+
+  sum = first_die + second_die + third_die + fourth_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} and a #{third_die} and a #{fourth_die} for a total of #{sum}."
+	
+  "<h1>5d4</h1>
+   <p>#{outcome}</p>"
+end
