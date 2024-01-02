88b3fc9f642a751b0735e2a23064ffce3116aa8a
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-01-02 17:04:26 +0000

    trying to link to 2d6 roll

--- dice.rb
+++ dice.rb
@@ -3,6 +3,7 @@ require "sinatra/reloader"
 
 get("/") do
   "<h1> Dice Roll </h1>"
+  "<a href="https://supreme-space-garbanzo-979j4rwr5pwjc7xj5-4567.app.github.dev/dice/2/6">Roll two 6-sided dice</a>"
 end
 
 get("/zebra") do
