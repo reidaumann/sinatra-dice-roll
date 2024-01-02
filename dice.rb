bc52c00280d21ea333f69a14887571936a60fcc7
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-01-02 17:07:06 +0000

    fix homepage links

--- dice.rb
+++ dice.rb
@@ -2,8 +2,13 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "<h1> Dice Roll </h1>"
-  "<a href="https://supreme-space-garbanzo-979j4rwr5pwjc7xj5-4567.app.github.dev/dice/2/6">Roll two 6-sided dice</a>"
+  "<h1> Dice Roll </h1>
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  </ul>"
 end
 
 get("/zebra") do
