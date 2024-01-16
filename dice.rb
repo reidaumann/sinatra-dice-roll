ae747ff2dd389445ad703843ce1c866a35dc66f3
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-01-16 19:37:55 +0000

    added yields for wrapper on each

--- dice.rb
+++ dice.rb
@@ -26,7 +26,7 @@ get("/dice/2/6") do
 	
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  erb(:two_six)
+  erb(:two_six, { :layout => :wrapper })
 end
 
 get("/dice/2/10") do
@@ -36,7 +36,7 @@ get("/dice/2/10") do
 	
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  erb(:two_ten)
+  erb(:two_ten, { :layout => :wrapper })
 end
 
 get("/dice/1/20") do
@@ -44,7 +44,7 @@ get("/dice/1/20") do
 	
   @outcome = "You rolled a #{first_die}."
 	
-  erb(:one_twenty)
+  erb(:one_twenty, { :layout => :wrapper })
 end
 
 get("/dice/5/4") do
@@ -58,5 +58,5 @@ get("/dice/5/4") do
 	
   @outcome = "You rolled a #{first_die} and a #{second_die} and a #{third_die} and a #{fourth_die} and a #{fifth_die} for a total of #{sum}."
 	
-  erb(:five_four)
+  erb(:five_four, { :layout => :wrapper })
 end
