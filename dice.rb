8e5d1c0218f5a4bd20e980b8f777cd1c4aa024d2
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-01-16 20:10:03 +0000

    added format to the rest of the examples

--- dice.rb
+++ dice.rb
@@ -32,33 +32,37 @@ get("/dice/2/6") do
 end
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @rolls = []
+
+  2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
 	
   erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  @die = rand(1..20)
+  @rolls = []
 
-  @outcome = "You rolled a #{@die}."
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
 
   erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
+  @rolls = []
 
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} and a #{third_die} and a #{fourth_die} and a #{fifth_die} for a total of #{sum}."
+  5.times do
+    die = rand(1..4)
+
+    @rolls.push(die)
+  end
 	
   erb(:five_four)
 end
