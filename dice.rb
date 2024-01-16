2842958373ea1a55da2b98716b7b34164b88d1f8
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-01-16 20:07:12 +0000

    changin two_six to one_hundred format

--- dice.rb
+++ dice.rb
@@ -20,12 +20,14 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
   erb(:two_six)
 end
 
