43c0409f40d89403100cb65075236fa486d7d68b
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-01-16 19:46:46 +0000

    adding layout with red and green to 1d20

--- dice.rb
+++ dice.rb
@@ -40,10 +40,10 @@ get("/dice/2/10") do
 end
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
-	
-  @outcome = "You rolled a #{first_die}."
-	
+  @die = rand(1..20)
+
+  @outcome = "You rolled a #{@die}."
+
   erb(:one_twenty)
 end
 
