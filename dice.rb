f25144de6251f94db5433ccf5383c153a0bccd28
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-01-16 19:25:53 +0000

    added @ to outcome

--- dice.rb
+++ dice.rb
@@ -24,7 +24,7 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
   erb(:two_six)
 end
